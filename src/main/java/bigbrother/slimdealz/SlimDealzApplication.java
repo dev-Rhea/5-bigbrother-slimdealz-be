5a65a6cad1388be663d75b8a38cf25250bba25c1
rhea <dev.rhea4444@gmail.com>
2024-08-16 21:30:21 +0900

    :sparkles: Query DSL 설정 추가

--- src/main/java/bigbrother/slimdealz/SlimDealzApplication.java
+++ src/main/java/bigbrother/slimdealz/SlimDealzApplication.java
@@ -2,10 +2,11 @@ package bigbrother.slimdealz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
+@EnableJpaAuditing
 public class SlimDealzApplication {
 
     public static void main(String[] args) {
