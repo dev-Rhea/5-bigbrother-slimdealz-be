159ca0b5fbdbab161c9573fc2e037b3aba36ba15
rhea <dev.rhea4444@gmail.com>
2024-08-17 23:52:03 +0900

    :sparkles: 검색 API

--- src/main/java/bigbrother/slimdealz/controller/ProductController.java
+++ src/main/java/bigbrother/slimdealz/controller/ProductController.java
@@ -1,4 +1,33 @@
 package bigbrother.slimdealz.controller;
 
+import bigbrother.slimdealz.entity.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class ProductController {
+    private final ProductController productService;
+
+    public ProductController(ProductController productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/search")
+    public List<Product> searchByKeyword(String keyword) {
+        return productService.searchByKeyword(keyword);
+    }
+
+    @GetMapping("/search/meaning")
+    public List<Product> searchByMeaning(String keyword) {
+        return productService.searchByMeaning(keyword);
+    }
+
+    @GetMapping("/search/converted")
+    public List<Product> searchByKoreanKeyword(String keyword) {
+        return productService.searchByKoreanKeyword(keyword);
+    }
 }
--- src/main/java/bigbrother/slimdealz/service/ProductService.java
+++ src/main/java/bigbrother/slimdealz/service/ProductService.java
@@ -1,4 +1,30 @@
 package bigbrother.slimdealz.service;
 
+import bigbrother.slimdealz.entity.Product;
+import bigbrother.slimdealz.repository.ProductRepository;
+import jakarta.persistence.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Cacheable(value = "productSearchCache", key = "#keyword")
+    public List<Product> searchByKeyword(String keyword) {
+        return productRepository.searchByKeyword(keyword);
+    }
+
+    public List<Product> searchByMeaning(String keyword) {
+        return productRepository.searchByMeaning(keyword);
+    }
+
+    public List<Product> searchByKoreanKeyword(String keyword) {
+        return productRepository.searchByKoreanKeyword(keyword);
+    }
 }
