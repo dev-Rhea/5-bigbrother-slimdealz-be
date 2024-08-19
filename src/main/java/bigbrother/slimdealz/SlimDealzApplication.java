d7d2bf7bf71645de81a27c2e6b24aa0c3da59837
rhea <dev.rhea4444@gmail.com>
2024-08-19 18:01:40 +0900

    :sparkles: security login form 미노출 처리

--- src/main/java/bigbrother/slimdealz/SlimDealzApplication.java
+++ src/main/java/bigbrother/slimdealz/SlimDealzApplication.java
@@ -2,11 +2,12 @@ package bigbrother.slimdealz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EntityScan(basePackages = "bigbrother.slimdealz.entity")
 public class SlimDealzApplication {
 
     public static void main(String[] args) {
--- src/main/java/bigbrother/slimdealz/config/SecurityConfig.java
+++ src/main/java/bigbrother/slimdealz/config/SecurityConfig.java
@@ -1,4 +1,28 @@
 package bigbrother.slimdealz.config;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests((authorizeRequests) -> {
+                    authorizeRequests.requestMatchers("/signup/**").authenticated();
+                    authorizeRequests.requestMatchers("signin/**").authenticated();
+                    authorizeRequests.anyRequest().permitAll();
+                })
+                .formLogin((formLogin) -> {
+                    formLogin.loginPage("/signin");
+                })
+                .build();
+    }
 }
