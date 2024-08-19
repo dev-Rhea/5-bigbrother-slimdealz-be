e59b2f31fca8bd3fc4471fe9c8e0f0edca0ac0f9
dev-Rhea <dev.rhea4444@gmail.com>
2024-08-20 01:03:43 +0900

    :sparkles: 검색 관련 data 설정 추가

--- src/main/java/bigbrother/slimdealz/controller/ProductController.java
+++ src/main/java/bigbrother/slimdealz/controller/ProductController.java
@@ -1,8 +1,7 @@
 package bigbrother.slimdealz.controller;
 
-import bigbrother.slimdealz.entity.Product;
+import bigbrother.slimdealz.entity.product.Product;
 import bigbrother.slimdealz.service.ProductService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
