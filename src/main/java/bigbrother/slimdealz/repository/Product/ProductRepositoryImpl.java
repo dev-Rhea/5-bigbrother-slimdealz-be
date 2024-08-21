c5569b0d93e0bda9d126ec9ae2a2d44d1b47e7f3
rhea <dev.rhea4444@gmail.com>
2024-08-21 15:34:43 +0900

    :bug: Fix : 경로 수정

--- src/main/java/bigbrother/slimdealz/repository/Product/ProductRepositoryImpl.java
+++ src/main/java/bigbrother/slimdealz/repository/Product/ProductRepositoryImpl.java
@@ -1,19 +1,22 @@
 package bigbrother.slimdealz.repository.Product;
 
 import bigbrother.slimdealz.entity.product.Product;
+import bigbrother.slimdealz.entity.product.QPrice;
+import bigbrother.slimdealz.entity.product.QProduct;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static bigbrother.slimdealz.entity.product.QPrice.price;
-import static bigbrother.slimdealz.entity.product.QProduct.product;
 
 @Repository
 @RequiredArgsConstructor
 public class ProductRepositoryImpl implements ProductRepositoryCustom {
     private final JPAQueryFactory queryFactory;
+
+    private final QProduct product = QProduct.product;
+    private final QPrice price = QPrice.price;
 
     @Override
     public List<Product> searchByKeyword(String keyword) {
