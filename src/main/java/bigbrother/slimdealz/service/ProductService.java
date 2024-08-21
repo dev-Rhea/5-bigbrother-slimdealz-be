b4d19db5ba1b250e0aeea582f92dde8e72e171b9
rhea <dev.rhea4444@gmail.com>
2024-08-21 20:02:25 +0900

    :art: Feat : exception, log 추가

--- src/main/java/bigbrother/slimdealz/service/ProductService.java
+++ src/main/java/bigbrother/slimdealz/service/ProductService.java
@@ -2,6 +2,8 @@ package bigbrother.slimdealz.service;
 
 import bigbrother.slimdealz.dto.ProductConverter;
 import bigbrother.slimdealz.dto.ProductDto;
+import bigbrother.slimdealz.exception.CustomErrorCode;
+import bigbrother.slimdealz.exception.CustomException;
 import bigbrother.slimdealz.repository.Product.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,16 +17,26 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     public List<ProductDto> searchProducts(String keyword) {
-        return productRepository.searchByKeyword(keyword)
+        List<ProductDto> products = productRepository.searchByKeyword(keyword)
                 .stream()
                 .map(ProductConverter::toProductDTO) //converter 를 통해 DTO 로 변환
                 .collect(Collectors.toList()); // stream의 변환된 요소들을 리스트로 반환
+
+        if(products.isEmpty()) {
+            throw new CustomException(CustomErrorCode.PRODUCT_NOT_FOUND);
+        }
+        return products;
     }
 
     public List<ProductDto> findLowestPriceProducts() {
-        return productRepository.findLowestPriceProducts()
+        List<ProductDto> products = productRepository.findLowestPriceProducts()
                 .stream()
                 .map(ProductConverter::toProductDTO)
                 .collect(Collectors.toList());
+
+        if(products.isEmpty()) {
+            throw new CustomException(CustomErrorCode.PRODUCT_NOT_FOUND);
+        }
+        return products;
     }
 }
