4da85ea23a153c2b1266f6fdbc32a5a9dee79045
rhea <dev.rhea4444@gmail.com>
2024-08-22 17:06:07 +0900

    :sparkles: Feat : 카테고리별 상품 목록 API

--- src/main/java/bigbrother/slimdealz/service/ProductService.java
+++ src/main/java/bigbrother/slimdealz/service/ProductService.java
@@ -17,6 +17,7 @@ import java.util.stream.Collectors;
 public class ProductService {
     private final ProductRepository productRepository;
 
+    // 상품 검색
     public List<ProductDto> searchProducts(String keyword) {
         List<ProductDto> products = productRepository.searchByKeyword(keyword)
                 .stream()
@@ -29,6 +30,7 @@ public class ProductService {
         return products;
     }
 
+    // 오늘의 최저가 상품 목록
     public List<ProductDto> findLowestPriceProducts() {
         List<ProductDto> products = productRepository.findLowestPriceProducts()
                 .stream()
@@ -41,6 +43,7 @@ public class ProductService {
         return products;
     }
 
+    // 상품 상세 페이지 정보
     public ProductDto getProductWithLowestPriceByName(String productName) {
         Product product = productRepository.findProductWithLowestPriceByName(productName);
 
@@ -48,5 +51,18 @@ public class ProductService {
             throw new CustomException(CustomErrorCode.PRODUCT_NOT_FOUND);
         }
         return ProductConverter.toProductDTO(product);
+    }
+
+    // 카테고리 별 상품 조회
+    public List<ProductDto> findByCategory(String category) {
+        List<ProductDto> products = productRepository.findByCategory(category)
+                .stream()
+                .map(ProductConverter::toProductDTO)
+                .collect(Collectors.toList());
+
+        if(products.isEmpty()) {
+            throw new CustomException(CustomErrorCode.PRODUCT_NOT_FOUND);
+        }
+        return products;
     }
 }
