de5cc821e19acb2e2fb039d1d5890bd9944d67ce
rhea <dev.rhea4444@gmail.com>
2024-08-23 15:06:48 +0900

    :art: Fix : annotation 누락된 부분 추가

--- src/main/java/bigbrother/slimdealz/repository/Product/PriceRepository.java
+++ src/main/java/bigbrother/slimdealz/repository/Product/PriceRepository.java
@@ -2,6 +2,8 @@ package bigbrother.slimdealz.repository.Product;
 
 import bigbrother.slimdealz.entity.product.Price;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PriceRepository extends JpaRepository<Price, Long> {
 }
