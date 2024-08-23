13dd115be746c5ecf6610bb7232d516db69841f7
dannycho1530 <dannycho1530@gmail.com>
2024-08-23 20:45:42 +0900

    Oauth2.0 배포 오류 수정 3

--- src/main/java/bigbrother/slimdealz/SlimDealzApplication.java
+++ src/main/java/bigbrother/slimdealz/SlimDealzApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "bigbrother.slimdealz")
 @EnableJpaAuditing
 @EntityScan(basePackages = "bigbrother.slimdealz.entity")
+
 public class SlimDealzApplication {
 
     public static void main(String[] args) {
