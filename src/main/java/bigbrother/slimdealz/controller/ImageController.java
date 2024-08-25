823476ec4bf22bfa0abcf23ee56697d9b1ece2a7
rhea <155525939+dev-Rhea@users.noreply.github.com>
2024-08-25 22:45:12 +0900

    :sparkles: Feat : 이미지 응답 API

---
+++ src/main/java/bigbrother/slimdealz/controller/ImageController.java
@@ -0,0 +1,35 @@
+package bigbrother.slimdealz.controller;
+
+import bigbrother.slimdealz.service.S3Service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/v1/image")
+public class ImageController {
+
+    private S3Service s3Service;
+
+    @GetMapping
+    public ResponseEntity<byte[]> getProductImage(@RequestParam String productName) throws IOException {
+        String fileName = s3Service.findImageName(productName);
+
+        byte[] imageData = s3Service.downloadImage(fileName);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(imageData);
+    }
+}
