8e9c4f88a3ad48cc582d09ac488392d108ecf230
rhea <155525939+dev-Rhea@users.noreply.github.com>
2024-08-27 17:37:09 +0900

    :art: Feat : 날짜 형식 변경

--- src/main/java/bigbrother/slimdealz/entity/BaseEntity.java
+++ src/main/java/bigbrother/slimdealz/entity/BaseEntity.java
@@ -10,6 +10,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 
