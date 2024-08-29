8300dd956bd5df0102f7772d272fbe042c016e30
rhea <155525939+dev-Rhea@users.noreply.github.com>
2024-08-29 13:52:44 +0900

    Fix : API 경로 수정

--- src/main/java/bigbrother/slimdealz/controller/ImageController.java
+++ src/main/java/bigbrother/slimdealz/controller/ImageController.java
@@ -18,7 +18,7 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@RequestMapping("/v1/image")
+@RequestMapping("/api/v1/image")
 public class ImageController {
 
     private final S3Service s3Service;
