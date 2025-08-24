d1e428e78849aafff162b1dd7830a162f821defd
dev-Rhea <dev.rhea4444@gmail.com>
2025-08-24 17:34:49 +0900

    refactor: 차트 응답 구조 수정

--- src/main/java/bigbrother/slimdealz/dto/product/ChartDto.java
+++ src/main/java/bigbrother/slimdealz/dto/product/ChartDto.java
@@ -1,17 +1,27 @@
 package bigbrother.slimdealz.dto.product;
 
+import bigbrother.slimdealz.entity.product.Price;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
-public class ChartDto {
-    private String productName;
-    private int lowestPrice;
-    private LocalDateTime date;
+public record ChartDto(
+    Long priceId,
+    int lowestPrice,
+    Long productId,
+    String productName,
+    LocalDateTime updatedAt
+) {
+    public static ChartDto of(Price price) {
+        return ChartDto.builder()
+            .priceId(price.getId())
+            .lowestPrice(price.getDiscountPrice())
+            .productId(price.getProduct().getId())
+            .productName(price.getProduct().getProductName())
+            .updatedAt(price.getUpdatedAt())
+            .build();
+    }
 }
